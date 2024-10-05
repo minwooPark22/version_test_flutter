bc96b4e1bcd4c4b49d136eeb1187b02bf59fe25a
Lee <cindy201156@gmail.com>
2024-10-05 21:13:17 +0900

    얌얌 9시 13분

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Google Sign In로그인!')),    //구글 로그인
+      appBar: AppBar(title: Text('Google Sign In')),    //구글 로그인
       body: Center(
         child: ElevatedButton(
           onPressed: () async {
